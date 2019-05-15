89b02ac14abaaa0a0554c6e953604a170d90d97b
gabriel <familia_maximo@hotmail.com>
2019-05-15 16:47:27 -0300

    trab CG

--- CG/primitives.py
+++ CG/primitives.py
@@ -1,19 +1,147 @@
 import pygame
 import math
+import numpy
+import sys
 
 # Cores
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 
-def retangulo(window, x1, y1, x2, y2, color):
-    bresenham(window, x1, y1, x2, y1, color)
-    bresenham(window, x1, y1, x1, y2, color)
-    bresenham(window, x1, y2, x2, y2, color)
-    bresenham(window, x2, y1, x2, y2, color)
+#retangulo sem restrição
+def retangulo(window, p1, p2,color):
+    bresenham2(window, p1[0], p1[1], p2[0], p1[1], color)
+    bresenham2(window, p1[0], p1[1], p1[0], p2[1], color)
+    bresenham2(window, p1[0], p2[1], p2[0], p2[1], color)
+    bresenham2(window, p2[0], p1[1], p2[0], p2[1], color)
+
+#retangulo com restrição
+def retangulo2(window, p1, p2,color):
+    bresenham(window, p1[0], p1[1], p2[0], p1[1], color)
+    bresenham(window, p1[0], p1[1], p1[0], p2[1], color)
+    bresenham(window, p1[0], p2[1], p2[0], p2[1], color)
+    bresenham(window, p2[0], p1[1], p2[0], p2[1], color)
 
 
-def bresenham(screen, x1, y1, x2, y2, color):
+#circulo sem restrição
+def circulo(overlay, x0, y0, x1,y1, color):
+    if y0 == y1:
+            if x0 == x1:
+                r=0
+            if x0 > x1:
+                r = x0 - x1
+            else:
+                r = x1 - x0
+    elif x0 == x1:
+        if x0 > x1:
+            r = y0 - y1
+        else:
+            r = y1 - y0
+    else:
+        if y1 > y0 and x1 > x0:
+            r = int(math.sqrt(((y1 - y0) * (y1 - y0)) + ((x1 - x0) * (x1 - x0))))
+        elif y1 > y0 and x1 < x0:
+            r = int(math.sqrt(((y1 - y0) * (y1 - y0)) + ((x0 - x1) * (x0 - x1))))
+        elif y1 < y0 and x1 > x0:
+            r = int(math.sqrt(((y0 - y1) * (y0 - y1)) + ((x0 - x1) * (x0 - x1))))
+        else:
+            r = int(math.sqrt(((y0 - y1) * (y0 - y1)) + ((x1 - x0) * (x1 - x0))))
+    x = 0
+    y = r
+    d = 1 - r
+    overlay.set_at((x + x0, y + y0), color)
+    overlay.set_at((x0 - y, x + y0), color)
+    overlay.set_at((x0 - y, y0 - x), color)
+    overlay.set_at((x0 - x, y0 - y), color)
+    overlay.set_at((x0 - x, y + y0), color)
+    overlay.set_at((x + x0, y0 - y), color)
+    overlay.set_at((y + x0, y0 - x), color)
+    overlay.set_at((y + x0, x + y0), color)
+    while y > x:
+        if d < 0:
+            d = d + (2 * x) + 3
+        else:
+            d = d + 2 * (x - y) + 5
+            y = y - 1
+        x = x + 1
+        overlay.set_at((x + x0, y + y0), color)
+        overlay.set_at((x0 - y, x + y0), color)
+        overlay.set_at((x0 - y, y0 - x), color)
+        overlay.set_at((x0 - x, y0 - y), color)
+        overlay.set_at((x0 - x, y + y0), color)
+        overlay.set_at((x + x0, y0 - y), color)
+        overlay.set_at((y + x0, y0 - x), color)
+        overlay.set_at((y + x0, x + y0), color)
+
+#circulo com restrição
+def circulo2(overlay, x0, y0, x1,y1, color):
+    if y0 == y1:
+            if x0 == x1:
+                r=0
+            if x0 > x1:
+                r = x0 - x1
+            else:
+                r = x1 - x0
+    elif x0 == x1:
+        if x0 > x1:
+            r = y0 - y1
+        else:
+            r = y1 - y0
+    else:
+        if y1 > y0 and x1 > x0:
+            r = int(math.sqrt(((y1 - y0) * (y1 - y0)) + ((x1 - x0) * (x1 - x0))))
+        elif y1 > y0 and x1 < x0:
+            r = int(math.sqrt(((y1 - y0) * (y1 - y0)) + ((x0 - x1) * (x0 - x1))))
+        elif y1 < y0 and x1 > x0:
+            r = int(math.sqrt(((y0 - y1) * (y0 - y1)) + ((x0 - x1) * (x0 - x1))))
+        else:
+            r = int(math.sqrt(((y0 - y1) * (y0 - y1)) + ((x1 - x0) * (x1 - x0))))
+    x = 0
+    y = r
+    d = 1 - r
+    if (x+x0)>50 and (y+y0)>50 and (x+x0)<800 and (y+y0)<600:
+        overlay.set_at((x + x0, y + y0), color)
+    if (x0-y)>50 and (x+y0)>50 and (x0-y)<800 and (x+y0)<600:
+        overlay.set_at((x0 - y, x + y0), color)
+    if (x0-y)>50 and (y0-x)>50 and (x0-y)<800 and (y0-x)<600:
+        overlay.set_at((x0 - y, y0 - x), color)
+    if (x0 -x)>50 and (y0-y)>50 and (x0 - x)<800 and (y0-y)<600:
+        overlay.set_at((x0 - x, y0 - y), color)
+    if (x0-x)>50 and (y+y0)>50 and (x0-x)<800 and (y0+y)<600:
+        overlay.set_at((x0 - x, y + y0), color)
+    if (x+x0)>50 and (y0-y)>50 and (x+x0)<800 and (y0-y)<600:
+        overlay.set_at((x + x0, y0 - y), color)
+    if (y+x0)>50 and (y0 -x)>50 and (y+x0)<800 and (y0-x):
+        overlay.set_at((y + x0, y0 - x), color)
+    if (y+x0)>50 and (x+y0)>50 and (y+x0)<800 and (x+y0):
+        overlay.set_at((y + x0, x + y0), color)
+    while y > x:
+        if d < 0:
+            d = d + (2 * x) + 3
+        else:
+            d = d + 2 * (x - y) + 5
+            y = y - 1
+        x = x + 1
+        if (x + x0) > 50 and (y + y0) > 50 and (x + x0) < 800 and (y + y0) < 600:
+            overlay.set_at((x + x0, y + y0), color)
+        if (x0 - y) > 50 and (x + y0) > 50 and (x0 - y) < 800 and (x + y0) < 600:
+            overlay.set_at((x0 - y, x + y0), color)
+        if (x0 - y) > 50 and (y0 - x)>50 and (x0 - y) < 800 and (y0 - x) < 600:
+            overlay.set_at((x0 - y, y0 - x), color)
+        if (x0 - x) > 50 and (y0 - y) > 50 and (x0 - x) < 800 and (y0 - y) < 600:
+            overlay.set_at((x0 - x, y0 - y), color)
+        if (x0 - x) > 50 and (y + y0) > 50 and (x0 - x) < 800 and (y0 + y) < 600:
+            overlay.set_at((x0 - x, y + y0), color)
+        if (x + x0) > 50 and (y0 - y) > 50 and (x + x0) < 800 and (y0 - y) < 600:
+            overlay.set_at((x + x0, y0 - y), color)
+        if (y + x0) > 50 and (y0 - x) > 50 and (y + x0) < 800 and (y0 - x):
+            overlay.set_at((y + x0, y0 - x), color)
+        if (y + x0) > 50 and (x + y0) > 50 and (y + x0) < 800 and (x + y0):
+            overlay.set_at((y + x0, x + y0), color)
+
+
+#bresenham sem restrição
+def bresenham2(screen, x1, y1, x2, y2, color):
 
     if x2 >= x1:
         dx = (x2-x1)  # estabelecimento do modulo de dx
@@ -72,10 +200,134 @@ def bresenham(screen, x1, y1, x2, y2, color):
                     d = d+incNE
                     y = y+incy
                     x = x+incx
-                screen.set_at((x, y), color)
+                screen.set_at((x, y), color)#sem re
+
+#bresenham com restrições
+def bresenham(screen, x1, y1, x2, y2, color):
+    if x2 >= x1:
+        dx = (x2 - x1)  # estabelecimento do modulo de dx
+        incx = 1  # da esquerda para a direita
+    else:
+        dx = (x1 - x2)
+        incx = -1
+
+    if y2 >= y1:
+        dy = (y2 - y1)  # modulo de dy
+        incy = 1  # de baixo para cima
+    else:
+        dy = (y1 - y2)
+        incy = -1  # de cima para baixo
+    x = x1
+    y = y1
+    if x1>=50 and y1>=50 and x1<=800 and y1<=600:
+        screen.set_at((x1, y1), color)
+
+    if dx == 0:
+        if incy > 0:
+            for y in range(y1, y2):  # Nao tenho ctz
+                if x>=50 and y>=50 and x<=800 and y<=600: #garantir que esteja dentro da area de desenho
+                    screen.set_at((x, y), color)
+        else:
+            for y in range(y2, y1):
+                if x >= 50 and y >= 50 and x <= 800 and y <= 600:  # garantir que esteja dentro da area de desenho
+                    screen.set_at((x, y), color)
+    elif dy == 0:
+        if incx > 0:
+            for x in range(x1, x2):
+                if x >= 50 and y >= 50 and x <= 800 and y <= 600:  # garantir que esteja dentro da area de desenho
+                    screen.set_at((x, y), color)
+        else:
+            for x in range(x2, x1):
+                if x >= 50 and y >= 50 and x <= 800 and y <= 600:  # garantir que esteja dentro da area de desenho
+                    screen.set_at((x, y), color)
+    else:
+        if dx >= dy:
+            d = (2 * dy - dx)
+            incE = 2 * dy
+            incNE = 2 * (dy - dx)
+            while x != x2:
+                if d <= 0:
+                    d = d + incE
+                    x = x + incx
+                else:
+                    d = d + incNE
+                    x = x + incx
+                    y = y + incy
+                if x >= 50 and y >= 50 and x <= 800 and y <= 600:  # garantir que esteja dentro da area de desenho
+                    screen.set_at((x, y), color)
+        else:
+            d = (2 * dx - dy)
+            incE = 2 * dx
+            incNE = 2 * (dx - dy)
+            while y != y2:
+                if d <= 0:
+                    d = d + incE
+                    y = y + incy
+                else:
+                    d = d + incNE
+                    y = y + incy
+                    x = x + incx
+                if x >= 50 and y >= 50 and x <= 800 and y <= 600:  # garantir que esteja dentro da area de desenho
+                    screen.set_at((x, y), color)
 
 
+#quadrado sem restrição
 def quadrado(window, x1, x2, y1, y2, color):
+    if y1 == y2 or x1 == x2:
+        if x1 == x2:
+            if x1 > x2:
+                distancia = y2 - y1
+            else:
+                distancia = y1 - y2
+
+            bresenham2(window, x2, y2, x2 + distancia, y2, color)
+            bresenham2(window, x2, y2, x2, y2 + distancia, color)
+            bresenham2(window, x2 + distancia, y2, x2 + distancia, y2 + distancia, color)
+            bresenham2(window, x2, y2 + distancia, x2 + distancia, y2 + distancia, color)
+        elif y1 == y2:
+            if y1 > y2:
+                distancia = x2 - x1
+            else:
+                distancia = x1 - x2
+
+            bresenham2(window, x2, y2, x2 + distancia, y2, color)
+            bresenham2(window, x2, y2, x2, y2 + distancia, color)
+            bresenham2(window, x2 + distancia, y2, x2 + distancia, y2 + distancia, color)
+            bresenham2(window, x2, y2 + distancia, x2 + distancia, y2 + distancia, color)
+    else:
+        if y2 > y1 and x2 > x1:
+            distancia = int(
+                math.sqrt(((y2 - y1) * (y2 - y1)) + ((x2 - x1) * (x2 - x1))))
+            bresenham2(window, x1, y1, x1 + distancia, y1, color)
+            bresenham2(window, x1, y1, x1, y1 + distancia, color)
+            bresenham2(window, x1 + distancia, y1, x1 + distancia, y1 + distancia, color)
+            bresenham2(window, x1, y1 + distancia, x1 + distancia, y1 + distancia, color)
+        elif y2 > y1 and x2 < x1:
+            distancia = int(
+                math.sqrt(((y2 - y1) * (y2 - y1)) + ((x1 - x2) * (x1 - x2))))
+            bresenham2(window, x1, y1, x1 - distancia, y1, color)
+            bresenham2(window, x1, y1, x1, y1 + distancia, color)
+            bresenham2(window, x1 - distancia, y1, x1 - distancia, y1 + distancia, color)
+            bresenham2(window, x1, y1 + distancia, x1 - distancia, y1 + distancia, color)
+        elif y2 < y1 and x2 > x1:
+            distancia = int(
+                math.sqrt(((y1 - y2) * (y1 - y2)) + ((x1 - x2) * (x1 - x2))))
+            bresenham2(window, x1, y1, x1 + distancia, y1, color)
+            bresenham2(window, x1, y1, x1, y1 - distancia, color)
+            bresenham2(window, x1 + distancia, y1, x1 + distancia, y1 - distancia, color)
+            bresenham2\
+                (window, x1, y1 - distancia, x1 + distancia, y1 - distancia, color)
+        else:
+            distancia = int(
+                math.sqrt(((y1 - y2) * (y1 - y2)) + ((x2 - x1) * (x2 - x1))))
+            bresenham2(window, x1, y1, x1 - distancia, y1, color)
+            bresenham2(window, x1, y1, x1, y1 - distancia, color)
+            bresenham2(window, x1 - distancia, y1, x1 - distancia, y1 - distancia, color)
+            bresenham2(window, x1, y1 - distancia, x1 - distancia, y1 - distancia, color)
+
+
+#quadrado com restrições
+def quadrado2(window, x1, y1, x2, y2, color):
     if y1 == y2 or x1 == x2:
         if x1 == x2:
             if x1 > x2:
@@ -106,7 +358,8 @@ def quadrado(window, x1, x2, y1, y2, color):
             bresenham(window, x1 + distancia, y1, x1 + distancia, y1 + distancia, color)
             bresenham(window, x1, y1 + distancia, x1 + distancia, y1 + distancia, color)
         elif y2 > y1 and x2 < x1:
-            distancia = int(math.sqrt(((y2 - y1) * (y2 - y1)) + ((x1 - x2) * (x1 - x2))))
+            distancia = int(
+                math.sqrt(((y2 - y1) * (y2 - y1)) + ((x1 - x2) * (x1 - x2))))
             bresenham(window, x1, y1, x1 - distancia, y1, color)
             bresenham(window, x1, y1, x1, y1 + distancia, color)
             bresenham(window, x1 - distancia, y1, x1 - distancia, y1 + distancia, color)
@@ -125,3 +378,38 @@ def quadrado(window, x1, x2, y1, y2, color):
             bresenham(window, x1, y1, x1, y1 - distancia, color)
             bresenham(window, x1 - distancia, y1, x1 - distancia, y1 - distancia, color)
             bresenham(window, x1, y1 - distancia, x1 - distancia, y1 - distancia, color)
+
+
+#bezier sem restrição
+def bezier(window, p1, p2, p3, color):
+    for t in numpy.arange(0, 1, 0.005):
+        omt = 1 - t
+        omt2 = omt * omt
+        omt3 = omt2 * omt
+        t2 = t * t
+        t3 = t2 * t
+        x = omt3 * p1[0] + ((3 * omt2) * t * p1[0]) + (3 * omt * t2 * p2[0]) + t3 * p3[0]
+        y = omt3 * p1[1] + ((3 * omt2) * t * p1[1]) + (3 * omt * t2 * p2[1]) + t3 * p3[1]
+        x = int(numpy.floor(x))
+        y = int(numpy.floor(y))
+
+        window.set_at((x, y), color)
+    pygame.display.flip()
+
+
+#bezier com restrições
+def bezier2(window, p1, p2, p3, color):
+    for t in numpy.arange(0, 1, 0.005):
+        omt = 1 - t
+        omt2 = omt * omt
+        omt3 = omt2 * omt
+        t2 = t * t
+        t3 = t2 * t
+        x = omt3 * p1[0] + ((3 * omt2) * t * p1[0]) + (3 * omt * t2 * p2[0]) + t3 * p3[0]
+        y = omt3 * p1[1] + ((3 * omt2) * t * p1[1]) + (3 * omt * t2 * p2[1]) + t3 * p3[1]
+        x = int(numpy.floor(x))
+        y = int(numpy.floor(y))
+
+        if x>50 and y>50 and y<800 and x<600:
+            window.set_at((x, y), color)
+    pygame.display.flip()
--- CG/window.py
+++ CG/window.py
@@ -1,7 +1,5 @@
-import pygame
-import sys
-import math
 from primitives import *
+
 
 screen_size = (800, 600)
 
@@ -11,83 +9,183 @@ foreground = black
 
 pygame.init()
 window = pygame.display.set_mode(screen_size)
-layer = pygame.surface.Surface(screen_size)
-layer.blit(window, (0, 0))
 
 pygame.display.set_caption('Trabalho computacao grafica')
 window.fill(background)
-layer.fill(white)
 pygame.display.flip()
 
-# Variaveis de controle
-clicked = False
-start = (0, 0)
+#Palletas 
+# Cor 0
+pygame.draw.rect(window, black, (0, 0, 50, 50))
+# Cor 1
+pygame.draw.rect(window, (169, 169, 169), (50, 0, 50, 50))
+# Cor 2
+pygame.draw.rect(window, (0, 0, 255), (100, 0, 50, 50))
+# Cor 3
+pygame.draw.rect(window, (255, 255, 0), (150, 0, 50, 50))
+# Cor 4
+pygame.draw.rect(window, (255, 0, 0), (200, 0, 50, 50))
+# Cor 5
+pygame.draw.rect(window, (0, 255, 0), (250, 0, 50, 50))
+# Cor 6
+pygame.draw.rect(window, (255, 105, 180), (300, 0, 50, 50))
+# Cor 7
+pygame.draw.rect(window, (255, 140, 0), (350, 0, 50, 50))
+# Cor 8
+pygame.draw.rect(window, (0, 191, 255), (400, 0, 50, 50))
+# Cor 9
+pygame.draw.rect(window, (255, 191, 0), (450, 0, 50, 50))
+# Cor 10
+pygame.draw.rect(window, (128, 0, 0), (500, 0, 50, 50))
+# Cor 11
+pygame.draw.rect(window, (138, 0, 138), (550, 0, 50, 50))
+# Cor 12
+pygame.draw.rect(window, (80, 200, 120), (600, 0, 50, 50))
+# Cor 13
+pygame.draw.rect(window, (190, 91, 89), (650, 0, 50, 50))
+# Cor 14
+pygame.draw.rect(window, (0, 139, 139), (700, 0, 50, 50))
+# Cor 15
+pygame.draw.rect(window, (166, 70, 62), (750, 0, 50, 50))
 
-
-primitiva = 0
 # interface:
 
-bresenham(window, 50, 50, 800, 50, black)
-bresenham(window, 50, 50, 50, 600, black)
+bresenham2(window, 50, 50, 800, 50, black)
+bresenham2(window, 50, 50, 50, 600, black)
+#Moldura da janela
+bresenham2(window,0,0,0,600,black)
+bresenham2(window,0,0,800,0,black)
+bresenham2(window,800,0,800,600,black)
+bresenham2(window,0,600,800,600,black)
 
 
 # para as primitivas
 # linha
 quadrado(window, 50, 50, 50, 100, black)
-bresenham(window, 10, 60, 40, 90, black)
+bresenham2(window, 10, 60, 40, 90, black)
 # retângulo
 quadrado(window, 50, 50, 100, 150, black)
-retangulo(window, 10, 120, 40, 140, black)
+retangulo(window, (10, 120), (40, 140), black)
 # quadrado
 quadrado(window, 50, 50, 150, 200, black)
 quadrado(window, 10, 40, 160, 180, black)
 # círculo
 quadrado(window, 50, 50, 200, 250, black)
-# spline ou bezier
-quadrado(window, 50, 50, 250, 300, black)
+circulo(window,24,325,40,325,black)
 # polilinha
 quadrado(window, 50, 50, 300, 350, black)
+bresenham2(window, 10, 260, 20, 280, black)
+bresenham2(window, 20, 280, 30, 260, black)
+bresenham2(window, 30, 260, 40, 280, black)
+# spline ou bezier
+quadrado(window, 50, 50, 250, 300, black)
+bezier(window,(10,210),(20,240),(45,240),black)
+# polilinha
+quadrado(window, 50, 50, 300, 350, black)
+bresenham2(window,10,260,20,280,black)
+bresenham2(window,20,280,30,260,black)
+bresenham2(window,30,260,40,280,black)
 
-#Palletas 
-# Cor 0
-quadrado(window, 0, 50, 50, 50, black)
-# Cor 1
-quadrado(window, 50, 100, 50, 50, black)
-# Cor 2
-quadrado(window, 100, 150, 50, 50, black)
-# Cor 3
-quadrado(window, 150, 200, 50, 50, black)
-# Cor 4
-quadrado(window, 200, 250, 50, 50, black)
-# Cor 5
-quadrado(window, 250, 300, 50, 50, black)
-# Cor 6
-quadrado(window, 300, 350, 50, 50, black)
-# Cor 7
-quadrado(window, 350, 400, 50, 50, black)
-# Cor 8
-quadrado(window, 400, 450, 50, 50, black)
-# Cor 9
-quadrado(window, 450, 500, 50, 50, black)
-# Cor 10
-quadrado(window, 500, 550, 50, 50, black)
-# Cor 11
-quadrado(window, 550, 600, 50, 50, black)
-# Cor 12
-quadrado(window, 600, 650, 50, 50, black)
-# Cor 13
-quadrado(window, 650, 700, 50, 50, black)
-# Cor 14
-quadrado(window, 700, 750, 50, 50, black)
-# Cor 15
-quadrado(window, 750, 800, 50, 50, black)
-
-#refresh (atualiza os pixels)
-pygame.display.flip()
-
-
-# try
-while 1:
+CorAtual = black
+primitiva=0
+controle=1#controle
+while True:
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                p1 = pygame.mouse.get_pos()
+                if primitiva == 0:
+                    if p1[0] > 0 and p1[1] > 50 and p1[0] < 50 and p1[1]<100:
+                        primitiva=1#linha
+                        continue
+                    if p1[0] > 0 and p1[1]>100 and p1[0] <50 and p1[1]< 150:
+                        primitiva=2#retangulo
+                        continue
+                    if p1[0]>0 and p1[1]>150 and p1[0]<50 and p1[1]<200:
+                        primitiva=3#quadrado
+                        continue
+                    if p1[0]>0 and p1[1]>200 and p1[0]<50 and p1[1]<250:
+                        primitiva=4#bezier
+                        continue
+                    if p1[0]>0 and p1[1]>250 and p1[0]<50 and p1[1]<300:
+                        primitiva=5#polilinha
+                        continue
+                    if p1[0]>0 and p1[1]>300 and p1[0]<50 and p1[1]<350:
+                        primitiva=6#circulo
+                        continue
+                if primitiva == 1:#linha
+                    if controle ==1:
+                        p2=pygame.mouse.get_pos()
+                        controle+=1
+                        continue
+                    if controle ==2:
+                        controle=1
+                        bresenham(window,p1[0],p1[1],p2[0],p2[1],black)
+                        primitiva =0
+                        continue
+                if primitiva == 2:#retangulo
+                    if controle ==1:
+                        p2=pygame.mouse.get_pos()
+                        controle+=1
+                        continue
+                    if controle ==2:
+                        controle=1
+                        retangulo2(window,p1,p2,black)
+                        primitiva=0
+                        continue
+                if primitiva ==3:#quadrado
+                    if controle ==1:
+                        p2=pygame.mouse.get_pos()
+                        controle+=1
+                        continue
+                    if controle ==2:
+                        controle=1
+                        quadrado2(window,p1[0],p1[1],p2[0],p2[1],black)
+                        primitiva=0
+                        continue
+                if primitiva ==4:#bezier
+                    if controle ==1:
+                        p2=pygame.mouse.get_pos()
+                        controle+=1
+                        continue
+                    if controle==2:
+                        p3=pygame.mouse.get_pos()
+                        controle+=1
+                        continue
+                    if controle ==3:
+                        controle=1
+                        primitiva = 0
+                        bezier2(window,p1,p3,p2,black)
+                        continue
+                if primitiva==5:#polilinha
+                    if controle==1:
+                        p2=pygame.mouse.get_pos()
+                        controle+=1
+                        bresenham(window,p1[0],p1[1],p2[0],p2[1],black)
+                        p3=[0,0]
+                        p3[0]=p2[0]
+                        p3[1]=p2[1]
+                        continue
+                    if controle==2:
+                        p2=pygame.mouse.get_pos()
+                        bresenham(window,p3[0],p3[1],p2[0],p2[1],black)
+                        p3[0]=p2[0]
+                        p3[1]=p2[1]
+                        if p2[0]>0 and p2[1]>50 and p2[0]<50 and p2[1]<600:
+                            primitiva=0
+                        continue
+                if primitiva==6:#circulo
+                    if controle==1:
+                        p2=pygame.mouse.get_pos()
+                        controle+=1
+                        continue
+                    if controle==2:
+                        controle=1
+                        circulo2(window,p1[0],p1[1],p2[0],p2[1],black)
+                        primitiva=0
+                        continue
+pygame.display.update()
